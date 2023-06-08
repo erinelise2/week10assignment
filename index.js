e855bf190a9aa6bd585890343f13ab90e72b5319
Erin Brickman <eelise187@Erins-MBP.attlocal.net>
2023-06-08 10:01:27 -0700

    updating page

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ myAnimalLabel.innerHTML = 'Animal'
 myForm.append(myAnimalLabel)
 myForm.append(myAnimalInput)
 
-document.body.append(myForm)
+document.getElementById('formId').append(myForm)
 
 let myLocationLabel = document.createElement('label')
 let myLocationInput = document.createElement('input')
@@ -40,22 +40,53 @@ myForm.style.backgroundColor = 'rgb(210, 55, 25)'
 myForm.style.color = 'white'
 myForm.style.fontFamily = 'courier'
 myForm.style.textAlign = 'center'
-
 myForm.style.display = 'flex'
 myForm.style.alignItems = 'center'
 myForm.style.flexDirection = 'column'
-myForm.style.maxWidth = '500px'
-
+myForm.style.maxWidth = '1000px'
 */
+
 /* 
 Creating table
 */
 
-let myTable = document.createElement('table')
+let myAnimalTable = document.createElementById('myAnimalTable');
+
+function createAnimalTable () {
+    let aName = document.getElementById('aName').value;
+    let aLocation = document.getElementById('aLocation').value;
+    let aDate = document.getElemendById('aDate').value;
+    let aNameCol = document.createElement('td');
+    let aLocationCol = document.createElement('td');
+    let aDateCol = document.createElement('td');
+
+    aNameCol.innerText = aName;
+    aLocation.innerText = aLocation;
+    aDateCol.innerText = aDate;
+
+    buttonCol.appendChild(button);
+
+    let formRow = myAnimalTable.insertRow(1);
+    formRow.appendChild(aNameCol);
+    formRow.appendChild(aLocationCol);
+    formRow.appendChild(aDateCol);
+    formRow.appendChild(buttonCol);
+
+document.getElementById('aName').value = "";
+document.getElementById('aLocation').value = "";
+document.getElementById('aDate').value = "";
+}
+
+function deleteRow(row) {
+    empTable.deleteRow(row.rowIndex);
+}
+
+/*
 let myTr = document.createElement('tr')
 let myTdAnimal = document.createElement('td')
 let myTdLocation = document.createElement('td')
 let myTdDate = document.createElement('td')
+
 
 myTdAnimal.innerHTML = 'Animal'
 myTdLocation.innerHTML = 'Location'
@@ -81,4 +112,4 @@ function add(){
   
 }
 console.log(myTable)
-
+*/
