7a2f772fd9a50d02d77d3827c00a1f1c01d9da02
Erin Brickman <eelise187@Erins-MBP.attlocal.net>
2023-06-08 10:34:28 -0700

    updating js page... still not filling in table

--- index.js
+++ index.js
@@ -12,20 +12,10 @@ function createAnimal () {
     let aNameCol = document.createElement('td');
     let aLocationCol = document.createElement('td');
     let aDateCol = document.createElement('td');
-    let buttonCol = document.createElement('td');
 
     aNameCol.innerText = aName;
     aLocationCol.innerText = aLocation;
     aDateCol.innerText = aDate;
-
-    let button = document.createElement('button');
-    button.classList = 'btn btn-warning';
-    button.innerText = 'Remove';
-    button.addEventListener('click', function () {
-        deleteRow(formRow);
-    });
-
-    buttonCol.appendChild(button);
 
     let formRow = myAnimalTable.insertRow(1);
     formRow.appendChild(aNameCol);
@@ -33,9 +23,9 @@ function createAnimal () {
     formRow.appendChild(aDateCol);
     formRow.appendChild(buttonCol);
 
-document.getElementById('aName').value = "";
-document.getElementById('aLocation').value = "";
-document.getElementById('aDate').value = "";
+    document.getElementById('aName').value = "";
+    document.getElementById('aLocation').value = "";
+    document.getElementById('aDate').value = "";
 }
 
 function deleteRow(row) {
